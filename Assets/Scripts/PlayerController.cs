5fcb97e5dd516812cef31405937ddeb19810eb6b
Molly <mollymcconaghy@gmail.com>
2023-10-15 18:36:04 +0100

    Add animations
    Player can move diagonally

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody2D myRB;
+    private Animator myAnim;
 
     [SerializeField]   //keep the variable private but editable
     private float speed;  //set the player speed
@@ -14,12 +15,15 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {   //multiply by the speed and time to add speed and time into it 
         myRB.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed;   //preset axis to use in unity to pull from the left and right buttons for keys don't need deltatime
-        Debug.Log(myRB.velocity);
+
+        myAnim.SetFloat("moveX", myRB.velocity.x);
+        myAnim.SetFloat("moveY", myRB.velocity.y);
     }
 }
