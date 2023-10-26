bc0698a09137290b2ad9ed8d83dcd2d56b667d69
Molly <mollymcconaghy@gmail.com>
2023-10-26 09:21:00 +0100

    enemy flashes when hit

--- Assets/Scripts/EnemyHealthManager.cs
+++ Assets/Scripts/EnemyHealthManager.cs
@@ -6,21 +6,45 @@ public class EnemyHealthManager : MonoBehaviour
 {
     public int enemyHealth;
     public int maxHealth;
+
+
+    private bool flashActive; //whenever the player gets hit, the player flashes
+    [SerializeField]
+    private float flashLength = 0.8f; //how long the player flashes for when hit
+    private SpriteRenderer goblinSprite; //the sprite renderer for the player
+
+    // [SerializeField]
+
+    // private AudioSource hurtSound;    
     // Start is called before the first frame update
     void Start()
     {
-        
+        goblinSprite = GetComponent<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(flashActive)
+        {
+            StartCoroutine(FlashPlayer());
+        }        
         
     }
+
+    IEnumerator FlashPlayer()
+    {
+        flashActive = false;
+        goblinSprite.color = new Color(goblinSprite.color.r, goblinSprite.color.g, goblinSprite.color.b, 0f); //set the alpha to 0
+        yield return new WaitForSeconds(flashLength * 0.2f); //multiply by 0.2f to make the flash shorter
+        goblinSprite.color = new Color(goblinSprite.color.r, goblinSprite.color.g, goblinSprite.color.b, 1f); //set the alpha to 1
+    }    
 
     public void HurtEnemy(int damageToGive)
     {
         enemyHealth -= damageToGive;
+        flashActive = true;
         if(enemyHealth <= 0)
         {
             Destroy(gameObject);
